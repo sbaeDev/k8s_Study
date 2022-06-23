2e6c5ae3f420d74df50996e0de5ee09c1388d394
sbaeDev <sbae.cloud@gmail.com>
2022-06-23 09:37:08 +0900

    correction of typo

--- master.sh
+++ master.sh
@@ -7,6 +7,6 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown \$(id -u):\$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-ehco "#######################################################################"
+echo "#######################################################################"
 echo "######################### save the token ##############################"
 echo "#######################################################################"
